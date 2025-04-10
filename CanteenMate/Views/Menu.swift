9b37104162890b11486231a2a4041f63e6ec109b
Hikaru <ahmed.nizhan@gmail.com>
2025-04-10 10:27:51 +0700

    Fix bug on menu page

--- CanteenMate/Views/Menu.swift
+++ CanteenMate/Views/Menu.swift
@@ -5,55 +5,55 @@ enum ActiveSheet {
     case first, second
 }
 
-
 struct Menu: View {
     @Environment(\.modelContext) private var modelContext
     @Query private var menus: [MenuModel]
-    @State private var isModalPresented = false
+    @State private var isAddMenuPresented = false
     @State private var selectedItem: MenuModel? = nil
-    @State private var activeSheet: ActiveSheet? = .first
+
     var body: some View {
         NavigationView {
             VStack {
                 List(menus) { item in
-                    Button(action: {
+                    HStack {
+                        Text(item.name)
+                        Spacer()
+                        Text("Rp\(item.price)")
+                    }
+                    .contentShape(Rectangle())
+                    .onTapGesture {
                         selectedItem = item
-                        isModalPresented = true
-                        activeSheet = .second
-                    }) {
-                        HStack {
-                            Text(item.name)
-                            Spacer()
-                            Text("Rp\(item.price)")
-                        }
-                    }
-                }
-                .toolbar {
-                    ToolbarItem(placement: .principal){
-                        Text("Menu")
-                            .font(.title)
-                            .fontWeight(.bold)
-                    }
-                    ToolbarItem(placement: .topBarTrailing){
-                        Button(action: {
-                            isModalPresented = true
-                            activeSheet = .first
-                        }) {
-                            Image(systemName: "plus")
-                                .font(.title2)
-                        }
-
                     }
                 }
             }
-            .sheet(isPresented: $isModalPresented) {
-                if self.activeSheet == .first {
-                    AddMenuModal(isPresented: $isModalPresented).presentationDetents([.medium])
-                }else{
-                    if selectedItem != nil {
-                        EditMenuModal(item: Binding($selectedItem)!, isPresented: $isModalPresented).presentationDetents([.medium])
+            .toolbar {
+                ToolbarItem(placement: .principal){
+                    Text("Menu")
+                        .font(.title)
+                        .fontWeight(.bold)
+                }
+                ToolbarItem(placement: .topBarTrailing){
+                    Button(action: {
+                        isAddMenuPresented = true
+                    }) {
+                        Image(systemName: "plus")
+                            .font(.title2)
                     }
                 }
+            }
+            .sheet(isPresented: $isAddMenuPresented) {
+                AddMenuModal(isPresented: $isAddMenuPresented)
+                    .presentationDetents([.medium])
+            }
+            .sheet(item: $selectedItem) { item in
+                EditMenuModal(item: Binding(get: { item }, set: { selectedItem = $0 }), isPresented: Binding(get: {
+                    selectedItem != nil
+                }, set: { newValue in
+                    if !newValue {
+                        selectedItem = nil
+                    }
+                }))
+                .presentationDetents([.medium])
             }
             .overlay {
                 if menus.isEmpty {
@@ -64,11 +64,11 @@ struct Menu: View {
                     })
                     .offset(y: -60)
                 }
-            }.background(Color(.systemGray6))
+            }
+            .background(Color(.systemGray6))
         }
     }
 }
-
 
 #Preview {
     Menu().modelContainer(for: MenuModel.self, inMemory: true)
